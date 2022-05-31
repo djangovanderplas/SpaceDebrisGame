3d33bd34e7ac4ac3996c477e19fb1ea5f1cc89b8
miguelillothegr8 <miguelcastrogracia@gmail.com>
2022-05-31 21:15:46 +0200

    For some reason this doesnt work as expected

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import sys,pygame
-
+import main as m
 # Configuration
 pygame.init()
 fps = 60
@@ -10,59 +10,65 @@ font = pygame.font.SysFont('Arial', 40)
 
 
 buttons = []
-#yeah dude trust me i totally know how to do oop and totally didnt yank this from the internet
+def main():
+    #yeah dude trust me i totally know how to do oop and totally didnt yank this from the internet  - miguelillo
 
-class Button():
-    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.onclickFunction = onclickFunction
-        self.onePress = onePress
-        self.alreadyPressed = False
+    class Button():
+        def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
+            self.x = x
+            self.y = y
+            self.width = width
+            print('1')
+            self.height = height
+            self.onclickFunction = onclickFunction
+            self.onePress = onePress
+            self.alreadyPressed = False
 
-        self.fillColors = {
-            'normal': '#ffffff',
-            'hover': '#666666',
-            'pressed': '#333333',
-        }
-        self.buttonSurface = pygame.Surface((self.width, self.height))
-        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+            self.fillColors = {
+                'normal': '#ffffff',
+                'hover': '#666666',
+                'pressed': '#333333',
+            }
+            self.buttonSurface = pygame.Surface((self.width, self.height))
+            self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
 
-        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
-        buttons.append(self)
-    def process(self):
-        mousePos = pygame.mouse.get_pos()
-        self.buttonSurface.fill(self.fillColors['normal'])
-        if self.buttonRect.collidepoint(mousePos):
-            self.buttonSurface.fill(self.fillColors['hover'])
-            if pygame.mouse.get_pressed(num_buttons=3)[0]:
-                self.buttonSurface.fill(self.fillColors['pressed'])
-                if self.onePress:
-                    self.onclickFunction()
-                elif not self.alreadyPressed:
-                    self.onclickFunction()
-                    self.alreadyPressed = True
-            else:
-                self.alreadyPressed = False
-        self.buttonSurface.blit(self.buttonSurf, [
-            self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
-            self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
-        ])
-        scr.blit(self.buttonSurface, self.buttonRect)
+            self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
+            buttons.append(self)
+        def process(self):
+            mousePos = pygame.mouse.get_pos()
+            self.buttonSurface.fill(self.fillColors['normal'])
+            if self.buttonRect.collidepoint(mousePos):
+                self.buttonSurface.fill(self.fillColors['hover'])
+                if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                    self.buttonSurface.fill(self.fillColors['pressed'])
+                    if self.onePress:
+                        self.onclickFunction()
+                    elif not self.alreadyPressed:
+                        self.onclickFunction()
+                        self.alreadyPressed = True
+                else:
+                    self.alreadyPressed = False
+            self.buttonSurface.blit(self.buttonSurf, [
+                self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
+                self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
+            ])
+            scr.blit(self.buttonSurface, self.buttonRect)
+    def startGame(Nsats):
+        m.main(Nsats)
 
-def pitilin():
-    print('pitilin')
-    return 0
-Button(30,30,400,100,'Pitilin',pitilin)
-while True:
-    scr.fill((20, 20, 20))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    for object in buttons:
-        object.process()
-    pygame.display.flip()
-    fpsclock.tick(fps)
+        return None 
+    Button(30,30,400,100,'Start the game in easy mode',startGame(5))
+    while True:
+        scr.fill((20, 20, 20))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        for object in buttons:
+            object.process()
+        pygame.display.flip()
+        fpsclock.tick(fps)
+
+if __name__ == '__main__':
+    main()
+
